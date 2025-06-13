4b81fbb7e9fdbbb625795320557dffa80f3fd3e4
Kevin Duran <ksgarzondura@gmail.com>
2025-06-13 13:22:50 -0500

    Se soluciono la altura del titulo del canvas

--- lib/pages/duran_welcome_page.dart
+++ lib/pages/duran_welcome_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
-class DuranWelcomePage extends StatelessWidget {
+class DuranWelcomePage extends StatefulWidget {
   final String title;
   final String subtitle;
   final String primaryButtonText;
@@ -10,13 +10,13 @@ class DuranWelcomePage extends StatelessWidget {
   final bool showSecondaryButton;
   final VoidCallback onPrimaryButtonClick;
   final VoidCallback onSecondaryButtonClick;
-  final SvgPicture svgPicture;
+  final SvgPicture image;
 
   const DuranWelcomePage({
     super.key,
     required this.title,
     required this.subtitle,
-    required this.svgPicture,
+    required this.image,
     this.primaryButtonText = "Primary",
     this.secondaryButtonText = "Secondary",
     this.showPrimaryButton = true,
@@ -28,6 +28,14 @@ class DuranWelcomePage extends StatelessWidget {
   static void _emptyCallback() {}
 
   @override
+  State<DuranWelcomePage> createState() => _DuranWelcomePageState();
+}
+
+class _DuranWelcomePageState extends State<DuranWelcomePage> {
+
+  Size size = Size(0, 0);
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
@@ -37,7 +45,7 @@ class DuranWelcomePage extends StatelessWidget {
           // Imagen (parte superior)
           Expanded(
             flex: 1,
-            child: svgPicture,
+            child: widget.image,
           ),
 
           // Parte inferior con canvas y botones
@@ -50,6 +58,7 @@ class DuranWelcomePage extends StatelessWidget {
                   child: CustomPaint(
                     painter: _WavePainter(
                       color: theme.colorScheme.primary,
+                      onSizeChange: (value) => setState(()=> size = value)
                     ),
                   ),
                 ),
@@ -57,78 +66,73 @@ class DuranWelcomePage extends StatelessWidget {
                 // Contenido sobre el canvas
                 Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: SafeArea(
-                    child: SingleChildScrollView(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          SizedBox(height: MediaQuery.of(context).size.height * 0.05),
-                          Text(
-                            title,
-                            textAlign: TextAlign.center,
-                            style: theme.textTheme.headlineLarge?.copyWith(
-                              fontWeight: FontWeight.bold,
-                              color: theme.colorScheme.surface,
-                            ),
-                          ),
-                          const SizedBox(height: 8),
-                          Text(
-                            subtitle,
-                            textAlign: TextAlign.center,
-                            style: theme.textTheme.bodyLarge?.copyWith(
-                              color: theme.colorScheme.surface.withOpacity(0.8),
-                            ),
-                          ),
-                          const SizedBox(height: 32),
-
-                          // Botón primario
-                          if (showPrimaryButton)
-                            SizedBox(
-                              width: double.infinity,
-                              height: 60,
-                              child: FilledButton(
-                                onPressed: onPrimaryButtonClick,
-                                style: FilledButton.styleFrom(
-                                  backgroundColor: theme.colorScheme.surface,
-                                  foregroundColor: theme.colorScheme.onSurface,
-                                ),
-                                child: Text(primaryButtonText),
-                              ),
-                            ),
-
-                          if (showSecondaryButton) const SizedBox(height: 16),
-
-                          // Botón secundario
-                          if (showSecondaryButton)
-                            SizedBox(
-                              width: double.infinity,
-                              height: 60,
-                              child: FilledButton(
-                                onPressed: onSecondaryButtonClick,
-                                style: FilledButton.styleFrom(
-                                  backgroundColor: theme.colorScheme.surface,
-                                  foregroundColor: theme.colorScheme.onSurface,
-                                ),
-                                child: Stack(
-                                  alignment: Alignment.center,
-                                  children: [
-                                    Align(
-                                      alignment: Alignment.center,
-                                      child: Text(secondaryButtonText),
-                                    ),
-                                    const Align(
-                                      alignment: Alignment.centerRight,
-                                      child: Icon(Icons.arrow_forward_ios),
-                                    )
-                                  ],
-                                ),
-                              ),
-                            ),
-
-                          const SizedBox(height: 24),
-                        ],
+                  child: Column(
+                    children: [
+                      SizedBox(height: size.height * 0.3),
+                      Text(
+                        widget.title,
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.headlineLarge?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          color: theme.colorScheme.surface,
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 8),
+                      Text(
+                        widget.subtitle,
+                        maxLines: 4,
+                        textAlign: TextAlign.center,
+                        style: theme.textTheme.bodyLarge?.copyWith(
+                          color: theme.colorScheme.surface.withOpacity(0.8),
+                        ),
+                      ),
+                      const Spacer(),
+
+                      // Botón primario
+                      if (widget.showPrimaryButton)
+                        SizedBox(
+                          width: double.infinity,
+                          height: 60,
+                          child: FilledButton(
+                            onPressed: widget.onPrimaryButtonClick,
+                            style: FilledButton.styleFrom(
+                              backgroundColor: theme.colorScheme.surface,
+                              foregroundColor: theme.colorScheme.onSurface,
+                            ),
+                            child: Text(widget.primaryButtonText),
+                          ),
+                        ),
+
+                      if (widget.showSecondaryButton) const SizedBox(height: 16),
+
+                      // Botón secundario
+                      if (widget.showSecondaryButton)
+                        SizedBox(
+                          width: double.infinity,
+                          height: 60,
+                          child: FilledButton(
+                            onPressed: widget.onSecondaryButtonClick,
+                            style: FilledButton.styleFrom(
+                              backgroundColor: theme.colorScheme.surface,
+                              foregroundColor: theme.colorScheme.onSurface,
+                            ),
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: [
+                                Align(
+                                  alignment: Alignment.center,
+                                  child: Text(widget.secondaryButtonText),
+                                ),
+                                const Align(
+                                  alignment: Alignment.centerRight,
+                                  child: Icon(Icons.arrow_forward_ios),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      const SizedBox(height: 24),
+                    ],
                   ),
                 ),
               ],
@@ -142,11 +146,13 @@ class DuranWelcomePage extends StatelessWidget {
 
 class _WavePainter extends CustomPainter {
   final Color color;
+  final void Function(Size size) onSizeChange;
 
-  _WavePainter({required this.color});
+  _WavePainter({required this.color, required this.onSizeChange});
 
   @override
   void paint(Canvas canvas, Size size) {
+    onSizeChange(size);
     final paint = Paint()..color = color;
 
     final path = Path();
